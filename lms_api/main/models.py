9ea4d9a591212c6e02788342949d08285fb28cef
sujan rokka <60314367+sujanrokka@users.noreply.github.com>
2024-05-28 18:23:33 +0545

    basic model created

--- lms_api/main/models.py
+++ lms_api/main/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+
+
+#teacher model
+class Teacher(models.Model):
+    full_name=models.CharField(max_length=100)
+    email=models.CharField(max_length=100)
+    password=models.CharField(max_length=100)
+    qualification=models.CharField(max_length=100)
+    mobile_no=models.CharField(max_length=100)
+    address=models.TextField()
+    
+ 
+    
+#course model
+class CourseCategory(models.Model):
+    title=models.CharField(max_length=150)
+    description=models.TextField()
+    
+
+#course model
+class Course(models.Model):
+    category=models.ForeignKey(CourseCategory,on_delete=models.CASCADE)
+    teacher=models.ForeignKey(Teacher,on_delete=models.CASCADE)
+    title=models.CharField(max_length=150)
+    description=models.TextField()
+    
+
+#student model
+class Student(models.Model):
+    full_name=models.CharField(max_length=100)
+    email=models.CharField(max_length=100)
+    password=models.CharField(max_length=100)
+    qualification=models.CharField(max_length=100)
+    mobile_no=models.CharField(max_length=100)
+    address=models.TextField()
+    interested_categories=models.TextField()
+    
+    
